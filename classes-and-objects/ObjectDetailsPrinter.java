62b08636e77c3a558202b9a66b83189da3125c0f
Rcaneda <rcaneda5@gmail.com>
2026-03-10 11:19:20 +0000

    Added my ObjectDetailsPrinter - Caneda

--- classes-and-objects/ObjectDetailsPrinter.java
+++ classes-and-objects/ObjectDetailsPrinter.java
@@ -69,8 +69,38 @@ public class ObjectDetailsPrinter {
         //Display Total Laptop using static method
         l2.displayTotalLaptop();
         
+        System.out.println("\n");
+
+        // This is the object instance created from the PC class
+        // This instance object was created by <Caneda>. This PC class was created by <Canillo>
+        System.out.println("\n This instance object was created by <Caneda>. This PC class was created by <Canillo>");
+        PC PC1 = new PC();
+        PC PC2 = new PC("Yellow");
+
+        //Access and Display Attributes: 
+        System.out.println("=== Attributes of P1 ===");
+        System.out.println("The GPU Manufacturer For PC1 is: " + PC1.GPUManufacturer);
+        System.out.println("The GPU Manufacturer For PC2 is: " + PC2.GPUManufacturer);
+
+        System.out.println("\nPC1 STATS")
+        //method without parameter
+        PC1.introduceSpecs();
+        // method with parameter
+        PC2.setCaseColor("Black"); 
+        //print of line 86
+        System.out.println("The new color of pc1 is: " + PC1.caseColor );
+
+        // static attribute 
+        System.out.println("The static attribute \"Socket type\"for PC1: "+ PC1.socketType);
+        // static method
+        PC1.displayTotalPC();
+    
+        
     }
 }
 
 // javac classes-and-objects-lab/Student.java classes-and-objects-lab/ObjectDetailsPrinter.java
+// java -cp classes-and-objects-lab ObjectDetailsPrinter
+
+// javac classes-and-objects-lab/PC.java classes-and-objects-lab/ObjectDetailsPrinter.java
 // java -cp classes-and-objects-lab ObjectDetailsPrinter
