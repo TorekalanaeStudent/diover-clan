b05ce18beb43022288f8fc5ac0c5773d4f688b86
cdejuras <clintnorbertdejuras@gmail.com>
2026-03-10 09:42:39 +0000

    added Mouse

--- classes-and-objects/Mouse.java
+++ classes-and-objects/Mouse.java
@@ -1,26 +1,57 @@
 public class Mouse {
 
+    // Attributes
     String model;
+    double price;
     String color;
+    String type;
 
-    public static String manufacturer = "Clint";
+    // Static Attribute
+    static int mouseTotal = 0;
 
-    public Mouse() {
-        this.model = "Logitech";
-        this.color = "Black";
+    // Default constructor
+    Mouse() {
+        model = "Logitech G403 HERO";
+        price = 15.99;
+        color = "Black";
+        type = "Wired";
+        mouseTotal++;
     }
 
-    public Mouse(String color) {
+    // Parameterized constructor
+    Mouse(String model, double price, String color, String type) {
+        this.model = model;
+        this.price = price;
         this.color = color;
-        this.model = "Logitech";
+        this.type = type;
+        mouseTotal++;
     }
 
-    public String getColor() {
-        return this.color;
+    // Overloaded constructor
+    Mouse(String model, double price) {
+        this.model = model;
+        this.price = price;
+        this.color = "0";
+        this.type = "0";
+        mouseTotal++;
     }
 
-    public static String getManufacturer() {
-        return manufacturer;
+    // Behavior without parameters
+    void displayDetails() {
+        System.out.println("Model: " + model);
+        System.out.println("Price: $" + price);
+        System.out.println("Color: " + color);
+        System.out.println("Type: " + type);
     }
 
+        // Behavior with parameters
+    void changeType(String newType){
+        type = newType;
+        System.out.println(model + " type has been updated to " + type);
+    }
+
+    // Static method
+    static void displayTotalMouse() {
+        System.out.println("Total Mouse Created: " + mouseTotal);
+    }
 }
--- classes-and-objects/ObjectDetailsPrinter.java
+++ classes-and-objects/ObjectDetailsPrinter.java
@@ -1,27 +1,29 @@
 public class ObjectDetailsPrinter {
-    public static void main(String[] args) {
-        // This is the object instance created from the Student class
-        // Object using default constructor
-        Student s1 = new Student();
+        public static void main(String[] args) {
+                // This is the object instance created from the Student class
+                        // Object using default constructor
+                                Book s1 = new Book();
 
-        // Object using parameterized constructor
-        Student s2 = new Student("Maria", 20, "BSCS");
+                                        // Object using parameterized constructor
+                                                Book s2 = new Book("Maria", 20, "BSCS");
 
-        // Object using overloaded constructor
-        Student s3 = new Student("Carlos", "BSIT");
+                                                        // Object using overloaded constructor
+                                                                Student s3 = new Student("Carlos", "BSIT");
 
-        // Print attributes and trigger behaviors
-        s1.introduce();
-        s2.introduce();
+                                                                        // Print attributes and trigger behaviors
+                                                                                s1.introduce();
+                                                                                        s2.introduce();
 
-        // Method with parameter
-        s2.updateCourse("BS Data Science");
+                                                                                                // Method with parameter
+                                                                                                        s2.updateCourse("BS Data Science");
 
-        // Static method
-        Student.displayTotalStudents();
-        
-    }
+                                                                                                                // Static method
+                                                                                                                        Student.displayTotalStudents();
+
+                                                                                                                                // This instance was made by <Clint>. 
+                                                                                                                                   }
+                                                                                                                                   }
+
+                                                                                                                                   // javac classes-and-objects-lab/Student.java classes-and-objects-lab/ObjectDetailsPrinter.java
+                                                                                                                                   // java -cp classes-and-objects-lab ObjectDetailsPrinter
 }
-
-// javac classes-and-objects-lab/Student.java classes-and-objects-lab/ObjectDetailsPrinter.java
-// java -cp classes-and-objects-lab ObjectDetailsPrinter
