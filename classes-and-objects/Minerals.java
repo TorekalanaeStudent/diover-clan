25f070dfa1b669f37ef6cf758e89d0a7c91f8ab9
Rcaneda <rcaneda5@gmail.com>
2026-03-10 20:29:01 +0800

    Update Minerals.java

--- classes-and-objects/Minerals.java
+++ classes-and-objects/Minerals.java
@@ -2,9 +2,8 @@ public class Minerals {
 
     // Attributes
     String mineralsName;
-
     String Color;
-    String mineLocation;
+    String mineLocation;  
     String Cords;
     int mineralsInInventory;
 
@@ -15,45 +14,50 @@ public class Minerals {
     // Default constructor
     Minerals() {
         this.mineralsName = "Diamond";
-        this.color = "Cyan";
-        this.Biome = "Mushroom islands";
+        this.Color = "Cyan";  
+        this.mineLocation = "Mushroom islands"; 
         this.Cords = "-1500, -30, 60000";
         this.mineralsInInventory = 64;
         totalMinerals++;
     }
-    // Parameteried constructor 
-    Minerals(String mineralsName, String color, String Biome, String Cords, int mineralsInInventory) {
+
+    // Parameterized constructor
+    Minerals(String mineralsName, String color, String mineLocation, String Cords, int mineralsInInventory) {
         this.mineralsName = mineralsName;
         this.Color = color;
-        this.Biome = Biome;
+        this.mineLocation = mineLocation; 
         this.Cords = Cords;
         this.mineralsInInventory = mineralsInInventory;
         totalMinerals++;
     }
+
     // Overloaded constructor
-    Minerals(String mineralsName,int mineralsInInventory) {
+    Minerals(String mineralsName, int mineralsInInventory) {
         this.mineralsName = mineralsName;
         this.Color = "NONE";
-        this.Biome = "NONE";
+        this.mineLocation = "NONE"; 
         this.Cords = "NONE";
         this.mineralsInInventory = mineralsInInventory;
         totalMinerals++;
     }
+
     // Behavior without parameters
     void displayMinerals() {
-        System.out.println("Minerals Name: "+ mineralsName);
-        System.out.println("Color: "+ color);
-        System.out.println("Biome: "+ Biome);
-        System.out.println("Cords: "+ Cords);
-        System.out.println("Minerals in inventory: "+mineralsInInventory);
+        System.out.println("Minerals Name: " + mineralsName);
+        System.out.println("Color: " + Color);
+        System.out.println("Mine Location: " + mineLocation); 
+        System.out.println("Cords: " + Cords);
+        System.out.println("Minerals in inventory: " + mineralsInInventory);
     }
+
     // Behavior with Parameters
     void updateMineral(String newMineral) {
-        System.out.println(mineralsMiner +"now has thrown out their "+mineralsName+" for "+newMinerals);
-        mineralsName = newMinerals;
+        System.out.println(mineralsMiner + " now has thrown out their " + mineralsName + " for " + newMineral); 
+        mineralsName = newMineral;
     }
+
     // Static method
-    static void displayTotalMineralsInInventory () {
-        System.out.println("Total Minerals In Inventory is "+totalMinerals);
+    static void displayTotalMineralsInInventory() {
+        System.out.println("Total Minerals In Inventory is " + totalMinerals);
     }
 }
