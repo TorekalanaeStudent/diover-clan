2525b06cdfc8c73f407f0f8191713afeb589c24a
rajangedoria-ctrl <rajan.gedoria@gmail.com>
2026-03-10 06:40:34 +0000

    Added Phone Class

--- classes-and-objects/Phone.java
+++ classes-and-objects/Phone.java
@@ -3,45 +3,77 @@ public class Phone {
     // Attributes
     String model;
     String color;
+    int storage;
 
     // Static attribute
-    static int amount = 0;
+    static int totalPhones = 0;
 
-    // Default Constructor
-    public Phone() {
-        model = "Galaxy S26";
-        color = "White";
-        amount++;
+    // Default constructor
+    Phone() {
+        model = "S26 Series";
+        color = "Black";
+        storage = 128;
+        totalPhones++;
     }
 
-    // Parameterized Constructor
-    public Phone(String model, String color) {
+    // Parameterized constructor
+    Phone(String model, String color, int storage) {
         this.model = model;
         this.color = color;
-        amount++
+        this.storage = storage;
+        totalPhones++;
     }
 
-    // Overloaded Constructor
-    public Phone(String model) {
+    // Overloaded constructor
+    Phone(String model, String color) {
         this.model = model;
         this.color = color;
-        amount++
+        this.storage = 128;
+        totalPhones++;
     }
 
     // Behavior without parameter
-    public void displayPhone() {
+    void displayPhoneInfo() {
         System.out.println("Model: " + model);
         System.out.println("Color: " + color);
+        System.out.println("Storage: " + storage + "GB");
     }
 
     // Behavior with parameter
-    public void changeColor(String newColor) {
+    void changeColor(String newColor) {
         color = newColor;
-        System.out.println("New color is: " + color);
+        System.out.println(model + " color changed to " + color);
     }
 
-    // Static Method
-    static void showBrand() {
-        System.out.println("Amount of phones available: " + amount);
+    // Static method
+    static void displayTotalPhones() {
+        System.out.println("Total Phones Created: " + totalPhones);
     }
-}
+
+
+        // Create objects using different constructors
+        Phone p1 = new Phone(); // default constructor
+        Phone p2 = new Phone("Galaxy S26", "White"); // parameterized
+        Phone p3 = new Phone("Galaxy A55", "Blue"); // overloaded
+
+       Access and display attributes
+        System.out.println("Phone 1: " + p1.model + ", " + p1.color + ", " + p1.storage);
+        System.out.println("Phone 2: " + p2.model + ", " + p2.color + ", " + p2.storage);
+        System.out.println("Phone 2: " + p3.model + ", " + p3.color + ", " + p3.storage);
+        
+        System.out.println(" ");
+
+
+        // behavior without parameter
+        p1.displayPhoneInfo();
+
+        System.out.println();
+
+        // behavior with parameter
+        p3.changeColor("Red");
+
+        System.out.println();
+
+        // Static usage
+        Phone.displayTotalPhones();
+    }
