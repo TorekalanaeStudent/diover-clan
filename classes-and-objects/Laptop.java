621aa5f4b0779e1f62c90ddfb3f513e97c82a28e
Margreleigne <ablismm@students.nu-laguna.edu.ph>
2026-03-10 12:41:33 +0800

    changes in laptop
    may dinagdag lang

--- classes-and-objects/Laptop.java
+++ classes-and-objects/Laptop.java
@@ -1,26 +1,51 @@
 public class Laptop {
+// Attributes
+String name;
+double price;
+String color;
+int stock;
+// Static Attribute
+ static int totalLaptop = 0;
+//Default constructor
+Laptop(){
+    name = "Lenovo";
+    price = 1000.00;
+    color = "Black";
+    stock = 20;
+    totalLaptop++;
+ }
+// parameterized constructor
+Laptop(String name, double price, String color, int stock){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+    this.stock = stock;
+    totalLaptop++;
+}
+// Overloaded constructor
+Laptop(String name, double price){
+    this.name = name;
+    this.price = price;
+    this.color = "NONE";
+    this.stock = 0;
+    totalLaptop++;
+}
+// Behavior without parameters
+    void displayDetails() {
+        System.out.println("Product: " + name);
+        System.out.println("Price: $" + price);
+        System.out.println("Color" + color );
+        System.out.println("Stock: " + stock);
+}
+ // Behavior with parameters
+ void applyDiscount(double percent) {
+        double discounted = price - (price * percent / 20);
+        price = discounted;
+        System.out.println(name + " is now on sale at $" + price + " after " + percent + "% discount.");
 
-    String model;
-    String color;
-
-    public static String manufacturer = "Ablis";
-
-    public Laptop() {
-        this.model = "Lenovo";
-        this.color = "White";
     }
-
-    public Laptop(String color) {
-        this.color = color;
-        this.model = "Lenovo";
+ // Static method
+    static void displayTotalLaptop() {
+        System.out.println("Total Laptop Created: " + totalLaptop);
     }
-
-    public String getColor() {
-        return this.color;
-    }
-
-    public static String getManufacturer() {
-        return manufacturer;
-    }
-
 }
