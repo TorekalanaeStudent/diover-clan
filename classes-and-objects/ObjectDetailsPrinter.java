e7a3fc2f03322d3d5dbc4afaef38a1bbcec7c8d0
Rich Matthew Malig <maligra@students.nu-laguna.edu.ph>
2026-03-10 13:07:35 +0000

    Added instance of Minerals

--- classes-and-objects/ObjectDetailsPrinter.java
+++ classes-and-objects/ObjectDetailsPrinter.java
@@ -288,6 +288,34 @@ public class ObjectDetailsPrinter {
         System.out.println("The static attribute \"Socket type\"for PC1: "+ PC1.socketType);
         // static method
         PC1.displayTotalPC();
+
+        // This object instance was created by <Malig>. This Minerals class was created by <Caneda>
+
+        // Object using default constructor
+        Minerals m1 = new Minerals();
+
+        // Object using parameterized constructor
+        Minerals m2 = new Minerals("Diamond", "Red", "Plains biome", "500 -43 124", 56);
+
+        // Print attributes
+        System.out.println();
+        System.out.println(m1.mineralsName);
+        System.out.println(m1.Color);
+        System.out.println(m1.mineLocation);
+        System.out.println(m1.Cords);
+        System.out.println(m1.mineralsInInventory);
+
+        // Method without parameters
+        m1.displayMinerals();
+        m2.displayMinerals();
+
+        // Method with parameter
+        m2.updateMineral("Emerald");
+        System.out.println();
+
+        // Static method and Static variable
+        Minerals.displayTotalMineralsInInventory();
+        System.out.println(Minerals.mineralsMiner); 
     }
 }
 
