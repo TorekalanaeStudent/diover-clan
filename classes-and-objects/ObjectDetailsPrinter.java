5bf1e9aff1dbad74521399413854b0d6427398e0
Navarro <navarrov@students.nu-laguna.edu.ph>
2026-03-10 09:04:14 +0000

    Added the instance of the Gun class

--- classes-and-objects/ObjectDetailsPrinter.java
+++ classes-and-objects/ObjectDetailsPrinter.java
@@ -44,6 +44,60 @@ public class ObjectDetailsPrinter {
         System.out.println("Keyboards Total: " + k1.totalKeyboard);
         // Display total keyboards using static method
         k2.displayTotalKeyboard();
+
+        // This is the object instance created from the Keyboard class
+        // This instance object was created by <Canillo>. This Keyboard class was created by <Onte>
+        System.out.println("\nThis instance object was created by <Canillo>. This Keyboard class was created by <Onte>"); // spacer
+        Keyboard k1 = new Keyboard(); // Object created using default constructor
+        Keyboard k2 = new Keyboard("Aula", 2499.00, "White", "Mechanical", 10); // Object created using parameterized constructor
+
+        // Attributes of k1
+        System.out.println("=== Attributes of k1 ===" 
+                + "\nBrand: " + k1.brand 
+                + "\nPrice: " + k1.price
+                + "\nColor: " + k1.color
+                + "\nType: " + k1.type
+                + "\nStock: " + k1.stock);
+
+        // usage for methods with parameter and without
+        System.out.println("\n=== METHODS ===");
+        k2.displayKeyboard(); // display of k2 (method without parameter)
+        k2.applyDiscount(20.0); // discount for k2 (method with parameter)
+
+        System.out.println("\n=== STATIC METHODS ===");
+        // Display total keyboards using static attribute
+        System.out.println("Keyboards Total: " + k1.totalKeyboard);
+        // Display total keyboards using static method
+        k2.displayTotalKeyboard();
+        
+
+     // This is the object instance created from the Gun class
+        // This instance object was created by <Navarro>. This Gun class was created by <Go>
+        System.out.println("\nThis instance object was created by <Navarro>. This Keyboard class was created by <Go>");
+        // Object using default constructor
+        Gun g1 = new Gun();
+        // Object using parameterized constructor
+        Gun g2 = new Gun("M4A1", 2001, 800, "5.56mm", "Colt");
+
+        // Attributes of g1
+        System.out.println("=== Attributes ==="
+                + "\nModel: " + g1.model
+                + "\nYear: " + g1.year
+                + "\nRounds Per Minute: " + g1.roundsPerMinute
+                + "\nCaliber: " + g1.caliber
+                + "\nManufacturer: " + g1.manufacturer);
+
+        // Methods
+        System.out.println("\n=== METHODS ===");
+        g2.displayInfo(); // method without parameter
+        g1.setInfo("Glock 17", 1982, 400, "9mm", "Glock"); // method with parameter
+        System.out.println("=== Updated Info ===");
+        g1.displayInfo(); // display updated g1
+
+        // Static
+        System.out.println("=== STATIC METHOD ===");
+        Gun.displayTotalStock(); // static method
+        
     }
 }
 
