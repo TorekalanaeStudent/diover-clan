b23c179a96f14b3cb8b4243c48a67630f563f7fd
Navarro <navarrov@students.nu-laguna.edu.ph>
2026-03-10 05:14:32 +0000

    added an instance of <CoffeeOrder.java>

--- classes-and-objects/ObjectDetailsPrinter.java
+++ classes-and-objects/ObjectDetailsPrinter.java
@@ -20,8 +20,25 @@ public class ObjectDetailsPrinter {
         // Static method
         Student.displayTotalStudents();
         
+        // Created instance of Products <Navarro>. This class was made by <Realon> "CoffeeOrder.Java"
+        // This is the object instance created from the Coffee class
+        // Object using parameterized constructor
+        CoffeeOrder c1 = new CoffeeOrder("Latte", "Small");
+
+        // Object using parameterized constructor
+        CoffeeOrder c2 = new CoffeeOrder("Espresso", "Medium");
+
+        // Object using parameterized constructor
+        CoffeeOrder c3 = new CoffeeOrder("Cappuccino", "Large");
+
+        // Print order and price
+        System.out.println(c1.getOrder() + " - " + c1.getPrice());
+        System.out.println(c2.getOrder() + " - " + c2.getPrice());
+        System.out.println(c3.getOrder() + " - " + c3.getPrice());
+        // Created instance of Products <Navarro>. This class was made by <Realon> "CoffeeOrder.Java"   
+
     }
 }
 
 // javac classes-and-objects-lab/Student.java classes-and-objects-lab/ObjectDetailsPrinter.java
-// java -cp classes-and-objects-lab ObjectDetailsPrinter
+// java -cp classes-and-objects-lab ObjectDetailsPrinterbjects-lab ObjectDetailsPrinter
