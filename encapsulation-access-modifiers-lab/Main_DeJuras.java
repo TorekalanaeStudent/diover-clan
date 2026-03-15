4f5851a78924ce4816df11d9ab0503ec11501f58
cdejuras <clintnorbertdejuras@gmail.com>
2026-03-15 14:51:09 +0000

    Added DeJuras_Main

--- encapsulation-access-modifiers-lab/Main_DeJuras.java
+++ encapsulation-access-modifiers-lab/Main_DeJuras.java
@@ -1,11 +1,33 @@
 public class Main_DeJuras {
 
     public static void main(String [] args){
-        //using Table by Trespeces
 
+        //using Basketball by Arban
+        // class has no behaviors
         
+        Basketball bBall1 = new Basketball();
+        Basketball bBall2 = new Basketball();
 
+        //setters
+        bBall1.setBrand("Wilson");
+        bBall1.setColor("Orange");
+        bBall1.setPrice(1500.00);
+        bBall1.setStock(10);
+        bBall1.setAvailable(true);
 
-        
+        bBall2.setBrand("Spalding");
+        bBall2.setColor("Black");
+        bBall2.setPrice(1000.00);
+        bBall2.setStock(0);
+        bBall2.setAvailable(false);
+
+        //getters
+
+        System.out.println("===== BALL SHOP =====");
+        bBall1.displayInfo();
+        System.out.println("=====================");
+        bBall2.displayInfo();
+        System.out.println("=====================");
+
     }
 }
