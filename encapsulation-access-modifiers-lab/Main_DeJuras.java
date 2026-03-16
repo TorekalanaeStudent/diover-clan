9148084fc09e1e2466e7592d84ef84ff9e17b75c
cdejuras <clintnorbertdejuras@gmail.com>
2026-03-16 14:49:05 +0000

    Main_DeJuras Encapsulation 2

--- encapsulation-access-modifiers-lab/Main_DeJuras.java
+++ encapsulation-access-modifiers-lab/Main_DeJuras.java
@@ -28,6 +28,37 @@ public class Main_DeJuras {
         System.out.println("=====================");
         bBall2.displayInfo();
         System.out.println("=====================");
+        
 
+        //using Table by Trespeces
+
+        Table table1 = new Table();
+        Table table2 = new Table();
+
+        //Setters
+        table1.setTableName("Nordic Edge Console");
+        table1.setTableColor("Walnut Brown");
+        table1.setTableMaterial("Solid Wood");
+        table1.setTablePrice(8999.00);
+        table1.setTableWidthCm(110.0);
+        table1.setTableLengthCm(40.0);
+        table1.setTableHeightCm(75.0);  
+
+        table2.setTableName ("Maxima 7 Billiard Table");
+        table2.setTableColor("Black");
+        table2.setTableMaterial("Wood Frame with Slate");
+        table2.setTablePrice(110000.00);
+        table2.setTableWidthCm(140.0);
+        table2.setTableLengthCm(254.0);
+        table2.setTableHeightCm(80.0); 
+        
+        //getters
+        System.out.println("====  TABLE SHOP ====");
+        table1.displayTableInfo();
+        table1.displayTotalTablesSold();
+        System.out.println("=====================");
+        table2.displayTableInfo();
+        System.out.println("=====================");
+        
     }
 }
