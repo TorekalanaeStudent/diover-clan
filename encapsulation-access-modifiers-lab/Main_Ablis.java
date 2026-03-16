ab87601cc277598636efa9597712b2fd7f3bf010
Margreleigne <ablismm@students.nu-laguna.edu.ph>
2026-03-16 22:54:20 +0800

    Update Main_Ablis.java

--- encapsulation-access-modifiers-lab/Main_Ablis.java
+++ encapsulation-access-modifiers-lab/Main_Ablis.java
@@ -7,6 +7,7 @@
 public class Main_Ablis {
   public static void main (String[] args) {
     System.out.println("=== Smartphone Class by Gedoria ===");
+    System.out.println("=== Created by Ablis ===");
         // Create objects
         Smartphone ph1 = new Smartphone("Samsung", "Galaxy S23", "09123456789");
         Smartphone ph2 = new Smartphone(); //default constructor
@@ -51,5 +52,55 @@ public class Main_Ablis {
 
         // static attribute
         System.out.println("\nTotal Smartphones Created: " + Smartphone.getTotalPhones());
+
+      // BANK ACCOUNT CLASS BY OBEDOZA
+    // second class created by Ablis
+   // note: AI was used to correct and explain errors
+        System.out.println("=== Bank Account Class by Obedoza ===");
+
+        // create objects for bank account
+        BankAccount ba1 = new BankAccount("Meme", "ACC001", 1000, true);
+        BankAccount ba2 = new BankAccount("", "ACC002", -500, false);
+
+        // ba1 details
+        System.out.println("\nACCOUNT 1 DETAILS");
+        System.out.println("Username: " + ba1.getUserName());
+        System.out.println("User ID: " + ba1.getUserId());
+        System.out.println("Balance: " + ba1.getAccBalance());
+        System.out.println("Active: " + ba1.isActive());
+
+        // ba2 details
+        System.out.println("\nACCOUNT 2 DETAILS");
+        System.out.println("Username: " + ba2.getUserName()); // becomes Unknown
+        System.out.println("User ID: " + ba2.getUserId());
+        System.out.println("Balance: " + ba2.getAccBalance()); // becomes 0
+        System.out.println("Active: " + ba2.isActive());
+
+        // setters
+        System.out.println("\nUpdating Account 2 Information:");
+        ba2.setUserName("Obedoza");
+        ba2.setAccBalance(1500);
+        ba2.setActive(true);
+
+        // getters after updating bank account
+        System.out.println("Updated Name: " + ba2.getUserName());
+        System.out.println("Updated Balance: " +ba2.getAccBalance());
+        System.out.println("Account Active: " + ba2.isActive());
+
+        // transactions of bank sccount
+        System.out.println("\nTRANSACTIONS: ");
+        ba1.deposit(500);
+        ba1.withdraw(200);
+
+        // bank account validation fules
+        System.out.println("\nVALIDATION RULES");
+        ba1.deposit(-100);     // invalid deposit
+        ba1.withdraw(5000);    // insufficient balance
+        ba1.withdraw(-50);     // invalid withdrawal
+
+        // final balance of bacnk account
+        System.out.println("\nFINAL BALANCE: ");
+        System.out.println("Account 1 Final Balance: " + ba1.getAccBalance());
+  
     }
 }
