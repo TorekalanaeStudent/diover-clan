2ca4b538a65d9957701f43ada62d4c10ea698721
SethPlaysBadly <seanethango217@gmail.com>
2026-03-17 14:04:15 +0800

    Add CrimsonWood_Go class extending Wood_Canillo

---
+++ inheritance-lab/sub_class/CrimsonWood_Go.java
@@ -0,0 +1,24 @@
+package sub_class;
+
+import parent_class.Wood_Canillo;
+
+public class CrimsonWood_Go extends Wood_Canillo {
+
+    public CrimsonWood_Go() {
+        super();
+        this.flammable = false;
+        this.woodType = "Crimson";
+        this.woodColor = "Maroon";
+        this.woodName = "Go's Crimson Wood";
+    }
+
+    @Override
+    public void burn() {
+        System.out.println(flammable ? woodName + " is burning." : woodName + " is flame resistant.");
+    }
+
+    @Override
+    public void knock() {
+        System.out.println("You knocked on the wood for 5 times to test its durability.");
+    }
+}
