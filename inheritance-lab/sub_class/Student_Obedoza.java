b7b7fe84cf285771a8fc5c679d0b551e79a87d22
Kyle Obedoza <obedozakyle@gmail.com>
2026-03-17 06:16:00 +0000

    Add Student_Obedoza class

--- inheritance-lab/sub_class/Student_Obedoza.java
+++ inheritance-lab/sub_class/Student_Obedoza.java
@@ -10,31 +10,26 @@ public class Student_Obedoza extends UserTypes_Caneda {
     private String nickName;
     private String iconColor;
 
-    public Student_Obedoza (String userName, String userCode, String userType, boolean userActivity) {
-        super();
+    public Student_Obedoza (String userName, String userCode, String userType, boolean userActivity, String nickName, String iconColor) {
+        super(userName, userCode, userType, userActivity);
 
-        // Attribues for super
-        this.userName = "Kyle_Obedoza";
-        this.userType = "Student";
-        this.userActivity = true;
-
-        this.nickName = "KoyKoy";
-        this.iconColor = "Green";
+        this.nickName = nickName;
+        this.iconColor = iconColor;
     }
 
     // Child Specific method 1
-    public void displyNickName() {
-        if (userActivity == true) {
-            System.out.println(nickname + "is active!");
+    public void displayNickName() {
+        if (userActivity) {
+            System.out.println(nickName + " is active!");
         } else {
-            System.out.println(nickname + "is offline!");
+            System.out.println(nickName + " is offline!");
         }
     }
 
     // Override parent method 
     @Override
     public void checkUserActivity() {
-        if (userActivity == true) {
+        if (userActivity) {
             System.out.println("Do not Disturb...");
         } else {
             System.out.println("Sleeping!");
