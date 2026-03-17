ae8455647bb30074376770c8c2d6278eb7697c0b
rajangedoria-ctrl <rajan.gedoria@gmail.com>
2026-03-17 08:08:29 +0000

    Adding Smartphone_Gedoria.java

---
+++ inheritance-lab/parent_class/Smartphone_Gedoria.java
@@ -0,0 +1,66 @@
+package parent_class;
+/*
+ * Parent Class
+ * This class is made by Rajan Kurt O. Gedoria
+ */
+
+public class Smartphone_Gedoria {
+
+// Attributes
+    String brand;
+    String model;
+    String operatingSystem;
+    boolean powerStatus;
+
+// Default Constructor
+    Smartphone_Gedoria() {
+        brand = "NA";
+        model = "NA";
+        operatingSystem = "NA";
+        powerStatus = false;
+    }
+
+// Parameterized Constructor
+    Smartphone_Gedoria(String brand, String model, String operatingSystem, boolean powerStatus) {
+        this.brand = brand;
+        this.model = model;
+        this.operatingSystem = operatingSystem;
+        this.powerStatus = powerStatus;
+    }
+
+// Behaviors
+
+    // Behavior #1: Validate Inputs
+    void checkDeviceInputs() {
+
+        // Check brand
+        if (brand == null) {
+            System.out.println("Device has no brand");
+        } else {
+            System.out.println("Brand is set");
+        }
+
+        // Check model
+        if (model == null) {
+            System.out.println("Device has no model");
+        } else {
+            System.out.println("Model is set");
+        }
+
+        // Check operating system
+        if (operatingSystem == null) {
+            System.out.println("No operating system specified");
+        } else {
+            System.out.println("Operating system is set");
+        }
+    }
+
+    // Behavior 2: Check Power Status
+    void checkPowerStatus() {
+        if (powerStatus == false) {
+            System.out.println("Smartphone is OFF");
+        } else {
+            System.out.println("Smartphone is ON");
+        }
+    }
+}
