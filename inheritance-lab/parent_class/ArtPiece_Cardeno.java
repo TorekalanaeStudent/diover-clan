8bd89d4c16dbbfb29eb0ac4f5c4bf411708cc47e
flcardeno <flcardeno@gmail.com>
2026-03-18 13:04:56 +0000

    Created ArtPiece_Cardeno parent class

---
+++ inheritance-lab/parent_class/ArtPiece_Cardeno.java
@@ -0,0 +1,34 @@
+package parent_class;
+
+public class ArtPiece_Cardeno {
+    // attributes 
+    protected String title;
+    protected String artist;
+    protected String medium; 
+    
+    // constructor - default
+    public ArtPiece_Cardeno() {
+        this.title = "The Starry Night";
+        this.artist = "Vincent Van Gogh";
+        this.medium = "oil on canvas";
+    }
+    // constructor - parmeterized
+    public ArtPiece_Cardeno(String title, String artist, String medium) {
+        this.title = title;
+        this.artist = artist;
+        this.medium = medium;
+    }
+
+    // behaviors
+    public void display() {
+        System.out.println(title + " by " + artist + " is now on display.");
+    }
+
+    public void evaluate() {
+        System.out.println(title + " is being evaluated.");
+    }
+
+    public void description() { // can be overridden
+        System.out.println("This art piece is titled " + title + " , created by " + artist + " using " + medium + ".");
+    }
+}
