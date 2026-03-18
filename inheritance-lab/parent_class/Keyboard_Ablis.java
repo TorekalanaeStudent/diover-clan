6b8be440a7092714db7fc11de40699ff672b5701
Margreleigne <ablismm@students.nu-laguna.edu.ph>
2026-03-18 04:41:54 +0000

    test keyboard_ablis.java

--- inheritance-lab/parent_class/Keyboard_Ablis.java
+++ inheritance-lab/parent_class/Keyboard_Ablis.java
@@ -1,53 +1,58 @@
 package parent_class;
 
 public class Keyboard_Ablis {
-    public static void main(String[] args) {
 
-        //keyboard attribute
-        protected String keyboardBrand;
-        protected String keyboardType; // mechanical, membrane, scissor switch
-        protected String connectionType; // wired, wireless
-        protected boolean plugStatus;
+    // kb attrqibutes
+    protected String keyboardBrand;
+    protected String keyboardType;
+    protected String connectionType;
+    protected boolean plugStatus;
 
-        // default constructor
-        public Keyboard_Ablis() {
-            this.keyboardBrand = "N/A";
-            this.keyboardType = "N/A";
-            this.connectionType = "N/A";
-            this.plugStatus = false;
-        }
+    // default constructor
+    public Keyboard_Ablis() {
+        this.keyboardBrand = "N/A";
+        this.keyboardType = "N/A";
+        this.connectionType = "N/A";
+        this.plugStatus = false;
+    }
 
-        // parameterized constructor
-        public Keyboard_Ablis(String keyboardBrand, String keyboardType, String connectionType, boolean plugStatus) {
-            this.keyboardBrand = keyboardBrand;
-            this.keyboardType = keyboardType;
-            this.connectionType = connectionType;
-            this.plugStatus = plugStatus;
-        }
-        // behaviour method
-        // behaviour #1: validate input
-        public void checkKeyboardInput() {
-            // check keyboard brand
-            if (keyboardBrand.equals("Logitech") || keyboardBrand.equals("Razer") || keyboardBrand.equals("Corsair")) {
-                system.out.println("Keyboard brand cannot be found.");
-            } else {
-                system.out.println("Keyboard brand found.");
-            }
-        }
-        // check connection type
-        if connectionType.equals("wired") || connectionType.equals("wireless")) {
-            system.out.println("Connection type is valid.");
+    // parameterized constructor
+    public Keyboard_Ablis(String keyboardBrand, String keyboardType, String connectionType, boolean plugStatus) {
+        this.keyboardBrand = keyboardBrand;
+        this.keyboardType = keyboardType;
+        this.connectionType = connectionType;
+        this.plugStatus = plugStatus;
+    }
+
+    // behaviour #1: validate input
+    public void checkKeyboardInput() {
+        // check keyboard brand (fixed swapped messages)
+        if (keyboardBrand.equals("Logitech") || keyboardBrand.equals("Razer") || keyboardBrand.equals("Corsair")) {
+            System.out.println("Keyboard brand found.");
         } else {
-            system.out.println("Connection type is not valid.");
+            System.out.println("Keyboard brand cannot be found.");
         }
 
-        // behaviour #2: check plug status
-        public void checkPlugStatus() {
-            if (plugStatus == true) {
-                system.out.println("Keyboard is PLUGGED IN.");
-            } else {
-                system.out.println("Keyboard is PLUGGED OUT.");
-            }
+        // check connection type (moved inside the method)
+        if (connectionType.equals("wired") || connectionType.equals("wireless")) {
+            System.out.println("Connection type is valid.");
+        } else {
+            System.out.println("Connection type is not valid.");
         }
+    }
+
+    // behaviour #2: check plug status
+    public void checkPlugStatus() {
+        if (plugStatus == true) {
+            System.out.println("Keyboard is PLUGGED IN.");
+        } else {
+            System.out.println("Keyboard is PLUGGED OUT.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Keyboard_Ablis kb = new Keyboard_Ablis("Logitech", "mechanical", "wired", true);
+        kb.checkKeyboardInput();
+        kb.checkPlugStatus();
     }
 }
