56c2263d97f6429b774e0e58498ef88b825bab38
rajangedoria-ctrl <rajan.gedoria@gmail.com>
2026-03-24 01:40:55 +0000

    Adding Handkerchief Class

---
+++ composition-lab/Handkerchief.java
@@ -0,0 +1,64 @@
+public class Handkerchief {
+    private String color;
+    private String size;
+    private boolean isUsed;
+
+// Constructor
+    public Handkerchief(String color, String size, boolean isUsed) {
+        this.color = color;
+        this.size = size;
+        this.isUsed = isSticky;
+    }
+
+     // Getters and Setters
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public void isUsed(boolean used) {
+        isUsed = used;
+    }
+
+     // Behavior methods
+    public void fold() {
+        System.out.println("Folding the handkerchief!");
+    }
+
+    public void use() {
+System.out.println("Handkerchief is being used!");
+    }
+
+    public void description() {
+        System.out.println("Handkerchief Details: ");
+        System.out.println("Color: " + color);
+        System.out.println("Size: " +  size);
+        System.out.println("Used: " + isUsed);
+
+    }
+
+        // Override toString()
+    @Override
+    public String toString() {
+        return "Handkerchief{" +
+                "color='" + color + '\'' +
+                ", size='" + size + '\'' +
+                ", isUsed=" + isUsed +
+                '}';
+    }
+}
