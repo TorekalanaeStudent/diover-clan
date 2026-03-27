028646bfe78793d3ab07c3408a7af5cd65575336
Margreleigne <ablismm@students.nu-laguna.edu.ph>
2026-03-27 08:03:00 +0000

    Changes in: tapegrip

--- composition-lab/sub_class/TapeGrip_Ablis.java
+++ composition-lab/sub_class/TapeGrip_Ablis.java
@@ -8,13 +8,13 @@ public class TapeGrip_Ablis extends Hands_Navarro {
 
     // default constructor
     public TapeGrip_Ablis() {
-        super("Default Hand", 75); // call parent constructor with default values
+        super("Default", "Medium", true); // call parent constructor with default values
         setTapeType("Loop"); // default tape type
     }
 
     // parameterized constructor
-    public TapeGrip_Ablis(String name, int length, String tapeType) {
-        super(name, length); // call parent constructor
+    public TapeGrip_Ablis(String skinTone, String handSize, boolean haveHands, String tapeType) {
+        super(skinTone, handSize, haveHands); // call parent constructor
         setTapeType(tapeType);
     }
 
@@ -38,10 +38,10 @@ public class TapeGrip_Ablis extends Hands_Navarro {
     }
 
     public void useHand() {
-       if (getLength() > 0 && getLength() < 200) { // validate hand length  
-            System.out.println(getName() + " is using their " + tapeType + " tape grip on their hand of length " + getLength() + "mm.");
+        if (hasHands()) {
+            System.out.println("Using " + tapeType + " tape grip with " + getHandSize() + " hands of skin tone " + getSkinTone() + ".");
         } else {
-            System.out.println(getName() + " cannot use their tape grip because the hand length is invalid.");
+            System.out.println("Cannot use tape grip because there are no hands available.");
         }
     }
 }
