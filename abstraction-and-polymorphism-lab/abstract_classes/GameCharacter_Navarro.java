b4cf7d146c33359ac74967442fb117c864c1a0b6
Navarro <navarrov@students.nu-laguna.edu.ph>
2026-04-07 09:45:05 +0800

    Made the recommended changes based on #680 Issue

--- abstraction-and-polymorphism-lab/abstract_classes/GameCharacter_Navarro.java
+++ abstraction-and-polymorphism-lab/abstract_classes/GameCharacter_Navarro.java
@@ -1,5 +1,9 @@
 package abstract_classes;
 
+/**
+ * @author Navarro, Vince Justine Navarro
+ * @StillLizard0106
+ */
 public abstract class GameCharacter_Navarro {
 		
 		// Attributes
@@ -13,10 +17,11 @@ public abstract class GameCharacter_Navarro {
 	    }
 	 
 	    // Abstract method - this will be overridden
-	    public abstract String getStatus();
-	    public abstract String getRole();
+	    public abstract String getStatus(); //This will check if the user is still alive or dead.
+	    public abstract String getRole(); //This will get your role/class
 	    
 	    // Concrete method
+		//This make it so you can attack someone or be the one who takes the damage.
 	    public void takeDamage(int amount) {
 	        health -= amount;
 	        System.out.println(name + " took " + amount + " damage! HP: " + health + " left!!");
