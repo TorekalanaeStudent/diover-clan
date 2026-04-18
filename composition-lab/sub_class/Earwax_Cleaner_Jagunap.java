f9cc5412c163221f8343222eb66f009f918c4c46
Janelle Jagunap <139051466+yumeko18@users.noreply.github.com>
2026-04-18 09:34:20 +0000

    added a subclass from earwax component class

---
+++ composition-lab/sub_class/Earwax_Cleaner_Jagunap.java
@@ -0,0 +1,52 @@
+package sub_class;
+
+import component_class.Earwax_Cardeno;
+
+/**
+ * Clearwax Cleaner removes or reduces earwax buildup
+ * 
+ * @author Jagunap
+ */
+public class Clearwax_Cleaner_Jagunap extends Earwax_Cardeno {
+
+    private String cleanerType;
+
+    public Clearwax_Cleaner_Jagunap() {
+        super(40, "Yellow", "Sticky", true);
+        setCleanerType();
+    }
+
+    public Clearwax_Cleaner_Jagunap(int amount, String color, String texture,
+                                    boolean sticky, String cleanerType) {
+        super(amount, color, texture, sticky);
+        setCleanerType(cleanerType);
+    }
+
+    public String getCleanerType() {
+        return cleanerType;
+    }
+
+    public void setCleanerType(String cleanerType) {
+        this.cleanerType = (cleanerType == null || cleanerType.isBlank())
+                ? "Drops"
+                : cleanerType;
+    }
+
+    // Overloading (default cleaner type)
+    public void setCleanerType() {
+        this.cleanerType = "Drops";
+    }
+
+    //@Override
+    public void showInfo() {
+        super.showInfo();
+
+        if (cleanerType.equalsIgnoreCase("Vacuum")) {
+            System.out.println("Cleaner Type: Vacuum → Removes earwax very effectively.");
+        } else if (cleanerType.equalsIgnoreCase("Spray")) {
+            System.out.println("Cleaner Type: Spray → Moderately cleans earwax.");
+        } else {
+            System.out.println("Cleaner Type: Drops → Slowly softens earwax.");
+        }
+    }
+}
