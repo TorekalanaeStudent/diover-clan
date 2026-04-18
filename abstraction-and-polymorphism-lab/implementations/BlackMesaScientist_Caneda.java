97763ab4cdb640d5f03a5002df78d6559f9e2e6c
Rcaneda <rcaneda5@gmail.com>
2026-04-18 19:02:48 +0800

    Update BlackMesaScientist_Caneda.java

--- abstraction-and-polymorphism-lab/implementations/BlackMesaScientist_Caneda.java
+++ abstraction-and-polymorphism-lab/implementations/BlackMesaScientist_Caneda.java
@@ -79,7 +79,6 @@ public void attack(GameCharacter_Navarro hostiles) {
 }
 
 //Overloaded Attack (actual attack)
-@Override
 public void attack(GameCharacter_Navarro hostiles, int playerDamage) {
     setPlayerDamage(playerDamage);
     System.out.println(getName() + " Shoots The Enemy " + hostiles.getName());
