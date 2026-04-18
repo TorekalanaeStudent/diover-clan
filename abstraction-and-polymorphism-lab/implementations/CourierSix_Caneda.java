2127df1cf53a83134bdcc0ae84225d619255c49a
Rcaneda <rcaneda5@gmail.com>
2026-04-18 17:44:23 +0800

    Update CourierSix_Caneda.java

--- abstraction-and-polymorphism-lab/implementations/CourierSix_Caneda.java
+++ abstraction-and-polymorphism-lab/implementations/CourierSix_Caneda.java
@@ -96,10 +96,10 @@ public void attack(GameCharacter_Navarro hostiles, int playerDamage) {
 }
 
 @Override
-public void defend(double playerDamage) {
-    int defendedPlayerAttack = (playerDamage*0.80);
+public void defend(int playerDamage) {
+    int defendedPlayerAttack = (int)(playerDamage*0.80);
     setIsGettingAttacked(true);
-    setHealthPoints(getHealthPoints() - defendedPlayerAttack);
+    setHealthPoints(getHealthPoints() - (int)defendedPlayerAttack);
     if(getHealthPoints() <= 0) {
         System.out.println(getName() + "Is Dead ");
     }
