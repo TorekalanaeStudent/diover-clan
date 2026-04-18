212de360a5ee92bb10b4f23fdae618a8f41ad43b
wong <trespecesgo@students.nu-laguna.edu.ph>
2026-04-18 13:12:20 +0000

    added checked and unchecked exceptions

---
+++ exceptions-and-errors-lab/MyNinjaTeam/exceptions/DivisionByZeroException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+/**
+ * @author Trespeces, Guillermo II O.
+ * @willj067
+ */
+
+// checked exception - must be declared or handled
+public class DivisionByZeroException extends Exception {
+
+    public DivisionByZeroException() {
+        super("Cannot divide by zero.");
+    }
+
+    public DivisionByZeroException(String message) {
+        super(message);
+    }
+}
