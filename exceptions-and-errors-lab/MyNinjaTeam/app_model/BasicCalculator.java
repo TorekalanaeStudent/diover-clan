33c8065388b86ef6ce2a2aab6c610c5faacb7927
Kyle Obedoza <obedozakyle@gmail.com>
2026-04-18 13:46:17 +0000

    add MyNinjaTeam

--- exceptions-and-errors-lab/MyNinjaTeam/app_model/BasicCalculator.java
+++ exceptions-and-errors-lab/MyNinjaTeam/app_model/BasicCalculator.java
@@ -22,23 +22,4 @@ public class BasicCalculator extends AbstractCalculator {
     public double multiplication(double a, double b) {
         return a * b;
     }
-
-    @Override
-    public double division(double a, double b) throws ArithmeticException {
-        if (b == 0 ) {
-            throw new ArithmeticException("Cannot be divided by zero!");
-        }
-        return a/b;
-    }
-
-    // Custom Validation method 
-    public void validate(double value) throws InvalidInputExceptions {
-        if (value == 0) {
-            throw new InvalidInputExceptions("Zero is not allowed!");
-        } 
-        if (value <0) {
-            throw new NegativeNumberException("Negative numbers are not allowed!");
-        }
-    }
-
 }
