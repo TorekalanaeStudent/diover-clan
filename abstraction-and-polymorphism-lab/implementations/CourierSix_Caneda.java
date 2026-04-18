f86037c82d051e1e9a30c35654e81ca39c2b2978
Rcaneda <rcaneda5@gmail.com>
2026-04-18 18:59:47 +0800

    Update CourierSix_Caneda.java

--- abstraction-and-polymorphism-lab/implementations/CourierSix_Caneda.java
+++ abstraction-and-polymorphism-lab/implementations/CourierSix_Caneda.java
@@ -93,7 +93,7 @@ public void attack(GameCharacter_Navarro hostiles) {
 public void attack(GameCharacter_Navarro hostiles, int playerDamage) {
     setPlayerDamage(playerDamage);
     System.out.println(getName() + " Shoots The Enemy " + hostiles.getName());
-    ((CombatSystem_Navarro)hostiles)
+    ((CombatSystem_Navarro)hostiles).defend();
 }
 
 @Override
