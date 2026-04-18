5f5fb726ba58095bd681be6ed12fb41318adf3ac
Kyle Obedoza <obedozakyle@gmail.com>
2026-04-18 12:20:53 +0000

    add MyNinjaTeam

---
+++ exceptions-and-errors-lab/MyNinjaTeam/app_model/BasicCalculator.java
@@ -0,0 +1,44 @@
+package model;
+
+
+public class BasicCalculator extends AbstractCalculator {
+
+    // Method overloading 
+    public int add(int a, int b) {
+        reutrn a + b;
+    }
+
+    @Override
+    public double addition(double a, double b) {
+        return a + b
+    }
+
+    @Override
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    @Override
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    @Override
+    public double division(double a, double b) throws ArithmeticException {
+        if (b == 0 ) {
+            throw new ArithmeticException("Cannot be divided by zero!")
+        }
+        return a/b;
+    }
+
+    // Custom Validation method 
+    public void validate(double value) throws InvalidInputExceptions {
+        if (value == 0) {
+            throw new InvalidInputExceptions("Zero is not allowed!");
+        } 
+        if (value <0) {
+            throw new NegativeNumberException("Negative numbers are not allowed!");
+        }
+    }
+
+}
