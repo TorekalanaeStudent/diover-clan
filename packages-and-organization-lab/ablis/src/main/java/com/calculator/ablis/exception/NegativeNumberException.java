b12fc11497b0a558e37f5470cd6e164a432c9a7f
Margreleigne <ablismm@students.nu-laguna.edu.ph>
2026-04-26 18:15:43 +0000

    packages-build-ablis

---
+++ packages-and-organization-lab/ablis/src/main/java/com/calculator/ablis/exception/NegativeNumberException.java
@@ -0,0 +1,13 @@
+/**
+ * @author: ABLIS, Margreleigne M.
+ * @matidagreat
+ * Date: 26/04/2026
+ */
+package com.calculator.ablis.exception;
+
+public class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+      
+}
---
+++ packages-and-organization-lab/ablis/src/main/java/com/calculator/ablis/exception/ZeroDivisionException.java
@@ -0,0 +1,13 @@
+/**
+ * @author: ABLIS, Margreleigne M.
+ * @matidagreat
+ * Date: 26/04/2026
+ */
+
+package com.calculator.ablis.exception;
+
+public class ZeroDivisionException extends Exception {
+    public ZeroDivisionException(String message) {
+        super(message);
+    }
+}
