f551d154549eff998aac7ac49876c80d684a455b
cdejuras <clintnorbertdejuras@gmail.com>
2026-04-26 14:17:39 +0000

    gradle clint

---
+++ packages-and-organization-lab/dejuras/src/main/java/com/calculator/dejuras/exception/InvalidInputException.java
@@ -0,0 +1,12 @@
+package com.calculator.dejuras.exception;
+
+/**
+ * De Juras, Clint Norbert P.
+ * @author @cdejuras
+ */
+public class InvalidInputException extends Exception {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
