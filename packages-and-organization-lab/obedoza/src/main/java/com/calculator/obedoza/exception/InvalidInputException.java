69618832ca7760c0b724049c8533691d93aff4de
Kyle Obedoza <obedozakyle@gmail.com>
2026-04-27 06:18:51 +0000

    add Obedoza

---
+++ packages-and-organization-lab/obedoza/src/main/java/com/calculator/obedoza/exception/InvalidInputException.java
@@ -0,0 +1,18 @@
+package com.calculator.obedoza.exception;
+
+/**
+ * @author Trespeces, Guillermo II O.
+ * @willj067
+ */
+
+// unchecked exception - extends Exception
+public class InvalidInputException extends Exception {
+
+    public InvalidInputException() {
+        super("Invalid input provided.");
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
