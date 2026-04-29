5a0e7440900caedf648b93cbfceaa4f5237aeb36
rajangedoria-ctrl <rajan.gedoria@gmail.com>
2026-04-30 00:28:24 +0800

    Goods

--- springboot-lab/gedoria/src/main/java/com/gedoria/springboot/controller/MeController.java
+++ springboot-lab/gedoria/src/main/java/com/gedoria/springboot/controller/MeController.java
@@ -3,11 +3,15 @@ package com.gedoria.springboot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class MeController {
 
     @GetMapping("/me")
     public Map<String, Object> getMe() {
+        Map<String, Object> response = new HashMap<>();
         response.put("name", "Rajan Kurt O. Gedoria");
         response.put("id", "2025-1021948");
         response.put("course", "CS");
