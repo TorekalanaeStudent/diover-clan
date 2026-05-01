81293856c70f717bd8525980c70944a14f7a67e1
Mary Angeline Nono <maryangelinebarnachea@gmail.com>
2026-05-01 19:38:14 +0800

    Revise comments in ProductController for clarity
    Updated comments to reflect personal perspective and improve clarity.

--- oop-databases-lab/nono/src/main/java/com/nono/app/controller/ProductController.java
+++ oop-databases-lab/nono/src/main/java/com/nono/app/controller/ProductController.java
@@ -14,23 +14,22 @@ import java.util.List;
 * Author Mary Angeline B. Nono
 * 
 * 1.What changed from previous lab?
-* -I moved from simple file handling or basic code to working with real databases. We learned how to connect our Spring Boot application to store and retrieve data properly, instead of just using variables or temporary memory.
+* -I moved from simple file handling or basic code to working with real databases. I learned how to connect our Spring Boot application to store and retrieve data properly, instead of just using variables or temporary memory.
 * 
 * 2. Why database is better?
-* - Data stays forever: Data is saved permanently even if we close the program.
+* - Data stays forever: Data is saved permanently even if I close the program.
 * - Organized: Information is stored in neat tables with rows and columns, making it easy to find.
-* - Safe & Secure: We can control who accesses the data and prevent mistakes.
+* - Safe & Secure: I can control who accesses the data and prevent mistakes.
 * - Handles large amounts: It can store thousands or millions of records easily without slowing down.
 *
 * 3.How JPA helped?
-*  -JPA (Java Persistence API) made our work much easier. Instead of writing long and complicated SQL queries manually, we just used Java code and annotations. It automatically created tables, inserted data, and handled connections for us. It saved a lot of time and reduced errors.
+*  -JPA (Java Persistence API) made our work much easier. Instead of writing long and complicated SQL queries manually, I just used Java code and annotations. It automatically created tables, inserted data, and handled connections for us. It saved a lot of time and reduced errors.
 *
 * 4.What changed when switching DB?
-*-When we switched from H2 to PostgreSQL:
-* 
-*- We had to change the URL, username, and password in  application.properties .
-*- We added the correct PostgreSQL Driver dependency.
-*- The way we write code stayed almost the same because JPA works with many databases. Only the configuration part changed.
+* -When I switched from H2 to PostgreSQL:
+* -I had to change the URL, username, and password in  application.properties .
+*- I added the correct PostgreSQL Driver dependency.
+*- The way I write code stayed almost the same because JPA works with many databases. Only the configuration part changed.
 **/
 
 @RestController
