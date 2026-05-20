1be36d80a3b750a3fdf56134244c5468696b5a74
rajangedoria-ctrl <rajan.gedoria@gmail.com>
2026-05-20 21:26:01 +0800

    Add Gedoria fullstack product CRUD app

---
+++ oop-fullstack-lab/gedoria/backend/src/test/java/com/fullstack/gedoria/GedoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.gedoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
